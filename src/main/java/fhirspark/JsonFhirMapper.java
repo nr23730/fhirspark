291a5e5a6ece38f81a9ec96e2361d68156e01c0c
Niklas Reimer <niklas@nr205.de>
2020-03-25 21:59:07 +0100

    adapt to new model for comments

--- src/main/java/fhirspark/JsonFhirMapper.java
+++ src/main/java/fhirspark/JsonFhirMapper.java
@@ -3,6 +3,7 @@ package fhirspark;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.ListIterator;
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
@@ -20,6 +21,7 @@ import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.gclient.ReferenceClientParam;
 import ca.uhn.fhir.rest.gclient.TokenClientParam;
+import net.minidev.json.JSONArray;
 
 public class JsonFhirMapper {
 
@@ -44,7 +46,11 @@ public class JsonFhirMapper {
 
             carePlan.addIdentifier(
                     new Identifier().setSystem("cbioportal").setValue((String) therapyRecommendation.get("id")));
-            carePlan.addNote(new Annotation().setText((String) therapyRecommendation.get("comment")));
+            List<Annotation> notes = new ArrayList<Annotation>();
+            ListIterator<Object> comments = ((JSONArray) therapyRecommendation.get("comment")).listIterator();
+            while (comments.hasNext())
+                notes.add(new Annotation().setText((String) comments.next()));
+            carePlan.setNote(notes);
 
             bundle.addEntry().setFullUrl(carePlan.getIdElement().getValue()).setResource(carePlan).getRequest()
                     .setUrl("CarePlan").setMethod(Bundle.HTTPVerb.POST);
@@ -64,36 +70,38 @@ public class JsonFhirMapper {
         jsonMap.put("id", patientId);
         jsonMap.put("therapyRecommendations", therapyRecommendations);
 
-
         Bundle bPatient = (Bundle) client.search().forResource(Patient.class)
                 .where(new TokenClientParam("identifier").exactly().systemAndCode("cbioportal", patientId))
                 .prettyPrint().execute();
 
         Patient patient = (Patient) bPatient.getEntryFirstRep().getResource();
 
-        if(patient == null)
+        if (patient == null)
             return "{}";
 
         Bundle bCarePlans = (Bundle) client.search().forResource(CarePlan.class)
                 .where(new ReferenceClientParam("subject").hasId(patient.getIdElement())).prettyPrint().execute();
 
         List<BundleEntryComponent> carePlans = bCarePlans.getEntry();
-        for(int i=0; i<carePlans.size(); i++) {
+        for (int i = 0; i < carePlans.size(); i++) {
             CarePlan carePlan = (CarePlan) carePlans.get(i).getResource();
-            
-            HashMap<String,Object> cPo = new HashMap<String,Object>();
-            HashMap<String,Object> cPi = new HashMap<String,Object>();
-            therapyRecommendations.put(String.valueOf(i), cPi);
-            cPi.put("id",carePlan.getIdentifierFirstRep().getValue());
-            cPi.put("comment",carePlan.getNoteFirstRep().getText());
 
-            ArrayList<HashMap<String,String>> treatments = new ArrayList<HashMap<String,String>>();
+            HashMap<String, Object> cPo = new HashMap<String, Object>();
+            HashMap<String, Object> cPi = new HashMap<String, Object>();
+            therapyRecommendations.put(String.valueOf(i), cPi);
+            cPi.put("id", carePlan.getIdentifierFirstRep().getValue());
+            ArrayList<String> comments = new ArrayList<String>();
+            for (Annotation annotation : carePlan.getNote())
+                comments.add(annotation.getText());
+            cPi.put("comment", comments);
+
+            ArrayList<HashMap<String, String>> treatments = new ArrayList<HashMap<String, String>>();
             cPi.put("treatments", treatments);
 
-            HashMap<String,ArrayList<HashMap<String,String>>> reasoning = new HashMap<String,ArrayList<HashMap<String,String>>>();
+            HashMap<String, ArrayList<HashMap<String, String>>> reasoning = new HashMap<String, ArrayList<HashMap<String, String>>>();
             cPi.put("reasoning", reasoning);
 
-            ArrayList<HashMap<String,String>> references = new ArrayList<HashMap<String,String>>();
+            ArrayList<HashMap<String, String>> references = new ArrayList<HashMap<String, String>>();
             cPi.put("references", references);
         }
 
