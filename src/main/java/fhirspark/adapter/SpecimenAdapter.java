36accc50cb7c6e962ddea114f04efaab4559c861
Niklas Reimer <niklas@nr205.de>
2020-06-05 12:58:00 +0200

    add specimen type

--- src/main/java/fhirspark/adapter/SpecimenAdapter.java
+++ src/main/java/fhirspark/adapter/SpecimenAdapter.java
@@ -5,12 +5,14 @@ import org.hl7.fhir.r4.model.Reference;
 import org.hl7.fhir.r4.model.Specimen;
 
 public class SpecimenAdapter {
-    
+
     public Specimen process(Reference patient, String specimen) {
         Specimen fhirSpecimen = new Specimen();
         fhirSpecimen.getMeta().addProfile("http://hl7.org/fhir/uv/genomics-reporting/StructureDefinition/specimen");
         fhirSpecimen.setSubject(patient);
         fhirSpecimen.addIdentifier(new Identifier().setSystem("https://cbioportal.org/specimen/").setValue(specimen));
+        fhirSpecimen.getType().addCoding().setSystem("http://terminology.hl7.org/CodeSystem/v2-0487").setCode("TUMOR")
+                .setDisplay("Tumor");
 
         return fhirSpecimen;
     }
